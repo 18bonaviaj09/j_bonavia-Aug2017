e34fbbb83ad37cb147b8ce3a772328e1b42e00b0
Teresa Cheng <cheng.teresa22@gmail.com>
2017-08-28 08:54:28 -0700

    PIKACCCHUU

--- Project 2/src/Project_2.java
+++ Project 2/src/Project_2.java
@@ -2,10 +2,9 @@
 // This is my second Java Project it will compare Print and Println
 // Created 8/22/2017
 public class Project_2 {
-
 	public static void main(String[] args) {
-		System.out.println("Hello This Is Project 2");
-		System.out.print("Hello This is Project 2");
+		System.out.println("Hey.");
+		System.out.print("Whats up?");
 	}
 
 }
