3aabf3e172f436a16bcd820ce9214b3bbe9af416
JEB <JEB@10.0.0.15>
2017-10-30 19:44:30 -0700

    sdfsddf

--- SplitString/src/Split.java
+++ SplitString/src/Split.java
@@ -2,13 +2,16 @@ import java.util.*;
 public class Split {
 
 	public static void main(String[] args) {
-
+		
+// Playing Around With Split
+		
 		System.out.println(Arrays.toString("Hello my name is Joseph!".split(" ")));
 		System.out.println(Arrays.toString("Iayamaytunaayfishay!".split("ay")));
 		System.out.println(Arrays.toString("lll".split("l")));
 		System.out.println(Arrays.toString("ljljl".split("l")));
+// Task One
 		System.out.println(sandwichReader("applespineapplesbreadlettustomatobaconmayohambreadcheese"));
-		System.out.println(sandwichReader("applespineapplesbreadbigmacsaucetwopattiespicklesbreadmustardtomatolettucebreadcheese"));
+// Task Two
 		System.out.println(sandwichReaderImproved("apples pineapples bread lettus tomato bacon mayo ham bread cheese"));
 	}
 	public static String sandwichReader(String sandwich) {
@@ -20,24 +23,18 @@ public class Split {
 		return ingredients;
 	}
 	public static String sandwichReaderImproved(String sandwich) {
-		String[] sandwichArray =  sandwich.split("bread");
+		String[] sandwichArray = sandwich.split(" ");
 		String ingredients = "";
-		for (int i = 1; i < sandwichArray.length - 1; i++) {
+		for (int i = 0; i < sandwichArray.length; i++) {
 			ingredients += sandwichArray[i];
 		}
-		String[] ingredientsArray = ingredients.split(" ");
-		for (int i = 0; i < sandwichArray.length; i++) {
-			ingredients += ingredientsArray[i];
+		
+		String[] sandwichArray2 =  ingredients.split("bread");
+		String ingredients2 = "";
+		for (int i = 1; i < sandwichArray2.length - 1; i++) {
+			ingredients2 += sandwichArray2[i];
 		}
-		return ingredients;
+		return ingredients2;
 	}
-	
-
-
-	//Your task pt 2:
-	/*Write a method that take in a string like "apples pineapples bread lettus tomato bacon mayo ham bread cheese" describing a sandwich
-	* use String.split to split up the sandwich at the spaces, " ", and return what's in the middle of the sandwich and ignores what's on the outside
-	* Again, what if it's a fancy sandwich with multiple pieces of bread?
-	*/
 
 }
