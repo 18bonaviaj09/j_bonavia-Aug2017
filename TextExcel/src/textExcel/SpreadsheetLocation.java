c9db752a3b47453a251bed219fa2b0ed844c5c0b
JEB <JEB@10.0.0.15>
2018-03-14 07:56:38 -0700

    I may have finally succeeded

---
+++ TextExcel/src/textExcel/SpreadsheetLocation.java
@@ -0,0 +1,32 @@
+package textExcel;
+
+//Update this file with your own code.
+
+public class SpreadsheetLocation implements Location {
+	private String cellName;
+	
+    @Override
+    public int getRow() {
+        String number = cellName.substring(1,cellName.length());
+        int row = 0;
+        while (!(number.equals("" + row))) {
+        	row++;
+        }
+    	return row - 1;
+    }
+
+    @Override
+    public int getCol()
+    {
+        char letter = cellName.charAt(0);
+        letter = Character.toLowerCase(letter);
+        return Character.getNumericValue(letter) - Character.getNumericValue('a');
+        
+        
+    }
+    
+    public SpreadsheetLocation(String cellName) {
+        this.cellName = cellName;
+    }
+
+}
---
+++ TextExcel/src/textExcel/TextExcel.java
@@ -0,0 +1,25 @@
+package textExcel;
+
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+// Update this file with your own code.
+
+public class TextExcel
+{
+
+	public static void main(String[] args)
+	{
+	    Scanner commandInput = new Scanner(System.in);
+	    Spreadsheet spreadsheet = new Spreadsheet();
+	    boolean contin = true;
+	    while (contin) {
+	    	String nextCommand = commandInput.nextLine();
+	    	if (nextCommand.equals("quit")) {
+	    		contin = false;
+	    	} else {
+	    		spreadsheet.processCommand(nextCommand);
+	    	}
+	    }
+	}
+}
